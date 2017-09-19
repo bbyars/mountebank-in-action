5629c49f72474b28bf364b50861bf184734e6e04
Brandon Byars <brandon.byars@gmail.com>
2017-09-18 20:17:33 -0500

    .NET Remoting example

---
+++ ch08/src/TownCrier-DotNetRemoting/TownCrier/Crier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TownCrier
+{
+    public class Crier : MarshalByRefObject
+    {
+        public delegate void AnnounceHandler(string topic);
+        public event AnnounceHandler AnnounceTopic;
+
+        public string Announce(string topic)
+        {
+            AnnounceTopic?.Invoke(topic);
+            return $"Hear ye! Hear ye! On this datetime {DateTime.Now} I hearby announce that {topic}";
+        }
+    }
+}
